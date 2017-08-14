2ca638aaa565d6d9bfc1c024be8b875925874e08
Valery Kharshats <valera.kh@gmail.com>
2017-08-13 22:43:03 -0400

    Use Typescript spread operator

--- src/app/store.reducer.ts
+++ src/app/store.reducer.ts
@@ -1,11 +1,10 @@
-import { Product } from './product';
 export const GET_STORE_ITEMS = 'GET_STORE_ITEMS';
 export const GET_STORE_ITEMS_SUCCESS = 'GET_STORE_ITEMS_SUCCESS';
 export const GET_STORE_ITEMS_ERROR = 'GET_STORE_ITEMS_ERROR';
 
 export function getStoreItems() {
   return {
-    type: GET_STORE_ITEMS
+    type: GET_STORE_ITEMS,
   };
 }
 
@@ -18,11 +17,11 @@ const initialState = {
 export function storeReducer(state = initialState, {type, payload}) {
   switch (type) {
     case GET_STORE_ITEMS:
-      return Object.assign({}, state, {pending: true, error: null});
+      return {...state, pending: true, error: null};
     case GET_STORE_ITEMS_SUCCESS:
-      return Object.assign({}, state, {data: payload, pending: false});
+      return {...state, data: payload, pending: false};
     case GET_STORE_ITEMS_ERROR:
-      return Object.assign({}, state, {pending: false, error: 'Error'});
+      return {...state, pending: false, error: 'Error'};
     default:
       return state;
   }
